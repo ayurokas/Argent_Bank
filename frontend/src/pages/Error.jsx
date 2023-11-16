a3409096eec22da3988fdac7f770295f8d0bd898
perso <elisa.github@gmail.com>
2023-11-16 20:45:07 +0100

    add error msg

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import imgError from "../assets/404Error.svg";
 
 export default function Error() {
     return (
         <div className='error'>
-            <h1>404</h1>
+            <img src={imgError} alt="error 404" width="600" height="600"/>
             <h2>Oups! La page que vous recherchez n'existe pas.</h2>
             <h3>
-                <Link to="/">Retourner sur la page d'accueil</Link>
+                <Link to="/" style={{ textDecoration: 'none', color: '#00bb76' }}>Retourner sur la page d'accueil</Link>
             </h3>
         </div>
     );
